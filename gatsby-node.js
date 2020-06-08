04fe24cf648a2b3ae7aa4783e9cc275c367d708b
jonaskay <jonaskay@iki.fi>
2020-06-08 12:52:59 +0300

    Create pages for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,27 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const content = await graphql(`
+    query {
+      posts: allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  content.data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `blog/${node.slug}`,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: { slug: node.slug },
+    })
+  })
+}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPost = ({ data }) => {
+  const post = data.contentfulBlogPost
+  const { author } = post
+
+  return (
+    <Layout>
+      <SEO title={post.title} />
+      <article>
+        <header>
+          <h1>{post.title}</h1>
+          <div>
+            By {author.name}, {author.title}
+          </div>
+          <div>{post.publishedAt}</div>
+          <div>Read time {post.readingTime} min</div>
+        </header>
+        <Image fluid={post.image.fluid} />
+        {documentToReactComponents(post.content.json)}
+      </article>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      publishedAt
+      image {
+        fluid(maxWidth: 2560) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+      author {
+        name
+        title
+      }
+      readingTime
+      content {
+        json
+      }
+    }
+  }
+`
+
+export default BlogPost
