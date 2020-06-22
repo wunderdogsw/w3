5043dd052be2be224a2a94f2759afd4ef0e8e5f4
jonaskay <jonaskay@iki.fi>
2020-06-22 11:16:36 +0300

    Remove console.log

--- src/components/logos-block.js
+++ src/components/logos-block.js
@@ -1,18 +1,14 @@
 import React from "react"
 import RichText from "./rich-text"
 
-const LogosBlock = ({ data }) => {
-  console.log(data.images)
-
-  return (
-    <section>
-      <h2>{data.heading}</h2>
-      {data.content && <RichText document={data.content.json} />}
-      {data.images.map(image => (
-        <img key={image.file.url} src={image.file.url} />
-      ))}
-    </section>
-  )
-}
+const LogosBlock = ({ data }) => (
+  <section>
+    <h2>{data.heading}</h2>
+    {data.content && <RichText document={data.content.json} />}
+    {data.images.map(image => (
+      <img key={image.file.url} src={image.file.url} />
+    ))}
+  </section>
+)
 
 export default LogosBlock
