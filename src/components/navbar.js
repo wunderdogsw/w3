250f35abb8ae523f2169b10acd64a7fb96b6ebeb
jonaskay <jonaskay@iki.fi>
2020-06-26 10:10:56 +0300

    Hide logo and menu when necessary

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import styles from "./navbar.module.css"
@@ -8,11 +8,28 @@ import Menu from "./menu"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const [logoHidden, setLogoHidden] = useState(false)
+
+  useEffect(() => {
+    const toggleLogo = () => setLogoHidden(window.scrollY > 20)
+    const hideMenu = () => setMenuOpen(false)
+
+    window.addEventListener("scroll", toggleLogo)
+    window.addEventListener("resize", hideMenu)
+
+    return () => {
+      window.removeEventListener("scroll", toggleLogo)
+      window.removeEventListener("resize", hideMenu)
+    }
+  }, [])
 
   return (
     <>
       <header className={styles.wrapper}>
-        <Link to="/" className={styles.logo}>
+        <Link
+          to="/"
+          className={`${styles.logo} ${logoHidden ? styles.hidden : ""}`}
+        >
           <Logo inverse={menuOpen} />
         </Link>
         <Toggle onClick={() => setMenuOpen(!menuOpen)} active={menuOpen} />
