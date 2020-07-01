005ffa28f1320660bcb15bead912f1c6e6c72498
jonaskay <jonaskay@iki.fi>
2020-07-01 08:59:50 +0300

    Extract text-content styles

--- src/components/section-block.js
+++ src/components/section-block.js
@@ -4,13 +4,19 @@ import Image from "gatsby-image"
 import styles from "./section-block.module.css"
 import RichText from "./rich-text"
 
-const renderImage = image => <Image fluid={image.fluid} />
+const renderImage = image => (
+  <div className={styles.image}>
+    <Image fluid={image.fluid} />
+  </div>
+)
 
 const SectionBlock = ({ data }) => (
   <section className={styles.wrapper}>
     {data.image && renderImage(data.image)}
-    <h2 className={styles.heading}>{data.heading}</h2>
-    <RichText document={data.content.json} />
+    <div className={styles.content}>
+      <h2 className={styles.heading}>{data.heading}</h2>
+      <RichText document={data.content.json} />
+    </div>
   </section>
 )
 
