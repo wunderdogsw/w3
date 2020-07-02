a268e20b85d88c0a70ec503aec6f7ff51e45ba3d
jonaskay <jonaskay@iki.fi>
2020-07-02 16:02:47 +0300

    Style logo sections

--- src/components/logos-block.js
+++ src/components/logos-block.js
@@ -1,13 +1,23 @@
 import React from "react"
+
+import styles from "./logos-block.module.css"
 import RichText from "./rich-text"
 
 const LogosBlock = ({ data }) => (
-  <section>
-    <h2>{data.heading}</h2>
-    {data.content && <RichText document={data.content.json} />}
-    {data.images.map(image => (
-      <img key={image.file.url} src={image.file.url} />
-    ))}
+  <section className={styles.wrapper}>
+    <div className={styles.content}>
+      <h2>{data.heading}</h2>
+      {data.content && <RichText document={data.content.json} />}
+    </div>
+    <div className={styles.list}>
+      <ul>
+        {data.images.map(image => (
+          <li>
+            <img key={image.file.url} src={image.file.url} />
+          </li>
+        ))}
+      </ul>
+    </div>
   </section>
 )
 
