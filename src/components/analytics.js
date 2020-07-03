1c01e2018929bc39592a8b4c091bf8698df894cb
jonaskay <jonaskay@iki.fi>
2020-07-03 13:35:34 +0300

    Add LinkedIn Insight Tag

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 import Facebook from "./facebook"
+import LinkedIn from "./linkedin"
 
 const Analytics = () => (
   <Helmet>
     <Facebook />
+    <LinkedIn />
   </Helmet>
 )
 
---
+++ src/components/linkedin.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+const LinkedIn = () => (
+  <>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        _linkedin_partner_id = "${GATSBY_LINKEDIN_PARTNER_ID}";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        </script><script type="text/javascript">
+        (function(){var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script");
+        b.type = "text/javascript";b.async = true;
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+        s.parentNode.insertBefore(b, s);})();
+      `,
+      }}
+    />
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style="display:none;"
+        alt=""
+        src={`https://px.ads.linkedin.com/collect/?pid=${GATSBY_LINKEDIN_PARTNER_ID}&fmt=gif`}
+      />
+    </noscript>
+  </>
+)
+
+export default LinkedIn
