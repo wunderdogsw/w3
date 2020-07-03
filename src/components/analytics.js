396c7d9d722feb17dfa2b05071365f7ab9bc066d
jonaskay <jonaskay@iki.fi>
2020-07-03 13:39:09 +0300

    Add HubSpot HS-scripts

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -3,11 +3,13 @@ import { Helmet } from "react-helmet"
 
 import Facebook from "./facebook"
 import LinkedIn from "./linkedin"
+import HubSpot from "./hubspot"
 
 const Analytics = () => (
   <Helmet>
     <Facebook />
     <LinkedIn />
+    <HubSpot />
   </Helmet>
 )
 
---
+++ src/components/hubspot.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const HubSpot = () => (
+  <script
+    type="text/javascript"
+    id="hs-script-loader"
+    async
+    defer
+    src={GATSBY_HUBSPOT_SCRIPT}
+  ></script>
+)
+
+export default HubSpot
