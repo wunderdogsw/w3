3a9df4fa980839f84b77f38e5429b95951e2c7dd
jonaskay <jonaskay@iki.fi>
2020-07-03 11:12:48 +0300

    Add descriptions and favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wunderdog`,
+    description: `Future-proof design & development`,
+    author: `@WunderdogOfcl`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/src/images/wunderdog-icon.png`,
       },
     },
     {
