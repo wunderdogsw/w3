924f01b949bb334aa764fc53a4c2539761c141cd
Chi Nguyen <chi.nguyen@wunderdog.fi>
2020-09-16 17:11:06 +0300

    put styling for the bar

--- src/components/cookie-consent-bar.js
+++ src/components/cookie-consent-bar.js
@@ -1,21 +1,47 @@
 import React from "react"
 import CookieConsent from "react-cookie-consent"
+import { useStaticQuery, graphql } from "gatsby"
+import styles from "./cookie-consent-bar.module.css"
 
 const CookieConsentBar = () => {
-  return (
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allContentfulCookieConsentBlock {
+          edges {
+            node {
+              privacyPolicy
+              consentText {
+                consentText
+              }
+              consentApprove
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const consents = data.allContentfulCookieConsentBlock.edges.map(node => node)
+  const content = consents.length && consents[0].node
+
+  return content ? (
     <CookieConsent
+      containerClasses={styles.wrapper}
+      contentClasses={styles.content}
+      buttonClasses={styles.button}
       location="bottom"
-      buttonText="Accept"
-      declineButtonText="Decline"
+      buttonText={content.consentApprove}
       onAccept={() => {
         window.dataLayer.push({
           event: "cookie_consent",
         })
       }}
     >
-      This site uses cookies ...
+      <span>{content.consentText.consentText}</span>
+      <a href="/privacy">{content.privacyPolicy}</a>
     </CookieConsent>
-  )
+  ) : null
 }
 
 export default CookieConsentBar
