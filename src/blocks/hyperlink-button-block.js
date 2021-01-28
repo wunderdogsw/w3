d5d62579a1b501d5f4888f1fcfed24f29eb9117e
Chi Nguyen <chi.nguyen@wunderdog.fi>
2021-01-28 12:51:17 +0200

    remove unwanted string interpolation

--- src/blocks/hyperlink-button-block.js
+++ src/blocks/hyperlink-button-block.js
@@ -5,7 +5,7 @@ import styles from "./hyperlink-button-block.module.css"
 const HyperlinkButtonBlock = ({ data }) => {
   return (
     <div className={`${styles.wrapper} ${styles[data.align]}`}>
-      <Link to={`${data.embeddedLink}`}>{data.textContent}</Link>
+      <Link to={data.embeddedLink}>{data.textContent}</Link>
     </div>
   )
 }
