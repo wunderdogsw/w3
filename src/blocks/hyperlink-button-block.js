c9d9949066107631cf2b801e5097387f322ae821
Chi Nguyen <chi.nguyen@wunderdog.fi>
2021-01-28 12:47:49 +0200

    add ability to have external link support to hyperlink block, add hover effect

--- src/blocks/hyperlink-button-block.js
+++ src/blocks/hyperlink-button-block.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Link from "../components/link"
 import styles from "./hyperlink-button-block.module.css"
-import { Link } from "gatsby"
+
 const HyperlinkButtonBlock = ({ data }) => {
   return (
     <div className={`${styles.wrapper} ${styles[data.align]}`}>
-      <Link to={`/${data.embeddedLink}`}>{data.textContent}</Link>
+      <Link to={`${data.embeddedLink}`}>{data.textContent}</Link>
     </div>
   )
 }
---
+++ src/components/link.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+
+const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
+  const isInternal = /^\/(?!\/)/.test(to)
+
+  if (isInternal) {
+    return (
+      <GatsbyLink
+        to={to}
+        activeClassName={activeClassName}
+        partiallyActive={partiallyActive}
+        {...other}
+      >
+        {children}
+      </GatsbyLink>
+    )
+  }
+
+  return (
+    <a href={to} {...other} target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  )
+}
+
+export default Link
