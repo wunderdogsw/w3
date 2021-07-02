3fa62a463e426f25b173d3d269da40ce4193ca06
Ido Schacham <ido.schacham@wunderdog.fi>
2021-07-02 09:18:59 +0200

    update Wunderdog home link aria-label

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
       <header className={styles.wrapper}>
         <Link
           to="/"
-          aria-label="Wunderdog logo"
+          aria-label="Wunderdog home"
           className={`${styles.logo} ${
             logoHidden && !menuOpen ? styles.hidden : ""
           }`}
