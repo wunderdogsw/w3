ff15165d2ca5731f0ae733a1c6950d31607371a6
Ido Schacham <ido.schacham@wunderdog.fi>
2021-07-14 15:08:53 +0200

    Update .eslintrc.js
    remove unused plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
-    // "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
   ],
   globals: {
